43b6c97528f5864cfa3838caf72fe1d59c7f24b0
Dmitriy Teplinskiy <99141331+MONSMIN@users.noreply.github.com>
2023-08-14 21:50:11 +0300

    add Author info

--- hw_project/quotes/views.py
+++ hw_project/quotes/views.py
@@ -1,8 +1,10 @@
+from bson import ObjectId
 from django.core.paginator import Paginator
 from django.shortcuts import render
 
 # Create your views here.
 from .utils import get_mongodb
+from .models import Author, Quote, Tag
 
 
 def main(request, page=1):
@@ -12,3 +14,9 @@ def main(request, page=1):
     paginator = Paginator(list(quotes), per_page)
     quotes_on_page = paginator.page(page)
     return render(request, 'quotes/index.html', context={'quotes': quotes_on_page})
+
+
+def author(request, id_):
+    db = get_mongodb()
+    author_info = db.authors.find({'_id': id_})
+    return render(request, 'quotes/author.html', context={'author': author_info})
