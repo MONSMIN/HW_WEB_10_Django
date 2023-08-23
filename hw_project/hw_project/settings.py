b710c6309b2ceecf6b18557bff36b4c0952cc4ee
Dmitriy Teplinskiy <99141331+MONSMIN@users.noreply.github.com>
2023-08-23 20:29:26 +0300

    update sc

--- hw_project/hw_project/settings.py
+++ hw_project/hw_project/settings.py
@@ -22,7 +22,7 @@ environ.Env.read_env(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-621f(+y#fwg0&01&5$e!-w+794s&(1tt!qyi557#lu!zfl10^s'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
